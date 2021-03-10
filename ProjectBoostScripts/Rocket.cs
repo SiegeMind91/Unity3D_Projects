b0a091c9c7296e6e65c239af326965d43832ea25
SiegeMind91 <56979128+SiegeMind91@users.noreply.github.com>
2021-03-10 14:06:31 -0600

    Add files via upload

---
+++ ProjectBoostScripts/Rocket.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 25f;
+    [SerializeField] float levelLoadDelay = 3f;
+    [SerializeField] AudioClip mainEngineAudio;
+    [SerializeField] AudioClip levelSuccessAudio;
+    [SerializeField] AudioClip deathAudio;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+
+    //Cached references
+    Rigidbody rocketRigidBody;
+    AudioSource audioSource;
+
+    enum State {  Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    bool collisionsAreEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == State.Alive)
+        {
+            Rotate();
+            Thrust();
+        }
+
+        //Debug builds are helpful because we can use debug keys
+        //Make sure to turn it on or off in the final build menu
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            //toggle collision
+            collisionsAreEnabled = !collisionsAreEnabled;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Guard condition to keep anything else from happening (also saves on processor power)
+        if (state != State.Alive || !collisionsAreEnabled)
+        {
+            return;
+        }
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //Nothing needs to happen here
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+        
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(levelSuccessAudio);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        mainEngineParticles.Stop();
+        audioSource.PlayOneShot(deathAudio);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay); 
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == (SceneManager.sceneCountInBuildSettings))
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+         
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ManuallyRotate(rcsThrust * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            ManuallyRotate(-rcsThrust * Time.deltaTime);
+        }
+    }
+
+    private void ManuallyRotate(float rotationThisFrame)
+    {
+        rocketRigidBody.freezeRotation = true; //take manual control of rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame);
+        rocketRigidBody.freezeRotation = false; //resume physics control of rotation
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rocketRigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); //Could add '* Time.deltaTime' to make it frame-independent, not necessary right now
+        //I'm checking to see if the source is already playing so that the audio doesn't layer
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngineAudio);
+        }
+
+         mainEngineParticles.Play();
+    }
+}
---
+++ TerminalHackerScript/Hacker.cs
@@ -0,0 +1,240 @@
+﻿using UnityEngine;
+using system = System; //Here I'm using an alias for the System namespace because there are two "Random" function definitions and this allows me to specify which I want to use
+
+public class Hacker : MonoBehaviour
+{
+    //Function variables
+    int level;
+    string password;
+    string scrambledPassword;
+    string[] level1Passwords = {"donuts", "jail", "holster", "uniform", "handcuffs", "bond"}; //Words to do with a police station
+    string[] level2Passwords = {"sobriety", "alphabet", "seatbelt", "pavement", "arrested", "citation"}; //Words to do with State Highway Patrol
+    string[] level3Passwords = {"investigation", "criminal", "cyberpunk", "fidelity", "anonymity", "firearm"}; //Words to do with the FBI
+    enum Screen { Instructions, MainMenu, Password, Win };
+    Screen currentScreen = Screen.Instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowFirstScreen();
+    }
+
+    void OnUserInput(string input)
+    {
+        if (currentScreen == Screen.Instructions)
+        {
+            ProcessInstructionsOptions();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            ProcessMainMenuOptions(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            ProcessPasswordGuess(input);
+        }
+        else if (currentScreen == Screen.Win)
+        {
+            ProcessWinOptions(input);
+        }
+        
+    }
+
+    private void ProcessWinOptions(string input)
+    {
+        if(input != null)
+        {
+            ShowMainMenuScreen();
+            currentScreen = Screen.MainMenu;
+        }
+    }
+
+    public void ProcessInstructionsOptions()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            ShowMainMenuScreen();
+            currentScreen = Screen.MainMenu;
+        }
+    }
+
+    void ProcessMainMenuOptions(string input)
+    {
+        if (input == "1" || input == "2" || input == "3")
+        {
+            level = int.Parse(input);
+            currentScreen = Screen.Password;
+            LoadPassword(input);
+            StartGame();
+        }
+        else if (input == "007")
+        {
+            Terminal.WriteLine("Good Evening, Mr. Bond");
+        }
+        else if (input == "69")
+        {
+            Terminal.WriteLine("Niiiice");
+        }
+        else if (input == "menu")
+        {
+            ShowMainMenuScreen();
+            currentScreen = Screen.MainMenu;
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+        }
+    }
+
+    void LoadPassword(string input)
+    {
+        int passwordSlot;
+
+        switch (input)
+        {
+            case "1":
+                passwordSlot = Random.Range(0, level1Passwords.Length);
+                password = level1Passwords[passwordSlot];
+                break;
+            case "2":
+                passwordSlot = Random.Range(0, level2Passwords.Length);
+                password = level2Passwords[passwordSlot];
+                break;
+            case "3":
+                passwordSlot = Random.Range(0, level3Passwords.Length);
+                password = level3Passwords[passwordSlot];
+                break;
+        }
+    }
+
+    private void ProcessPasswordGuess(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenuScreen();
+            currentScreen = Screen.MainMenu;
+        }
+        else if (input == password)
+        {
+            Terminal.ClearScreen();
+            currentScreen = Screen.Win;
+            switch (level)
+            {
+                case 1:
+                    ShowPoliceWinScreen();
+                    break;
+                case 2:
+                    ShowSHPWinScreen();
+                    break;
+                case 3:
+                    ShowFBIWinScreen();
+                    break;
+            }
+        }
+        else
+        {
+            Terminal.WriteLine("Password incorrect...please try again");
+        }
+
+    }
+
+    //functions for early in the game, but afterwards aren't used again
+    private void StartGame()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You have chosen level " + level);
+        scrambledPassword = ScrambleWord(password);
+        if(scrambledPassword == password) { scrambledPassword = ScrambleWord(password); } //Had to add a check due to some shorter words coming back unscrambled
+        Terminal.WriteLine("47.pwHlpr() returned " + scrambledPassword);
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Please enter your password");
+    }
+
+    public void ShowFirstScreen()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello Agent 47");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Your mission, should you choose to ");
+        Terminal.WriteLine("accept it, is to hack into one of 3");
+        Terminal.WriteLine("databases and discover intel that may");
+        Terminal.WriteLine("prove useful to our operatives.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Be warned, each level has a harder set of encryption.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("But with risk, comes great reward...");
+    }
+
+    public void ShowMainMenuScreen()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("");
+        Terminal.WriteLine("1 - Local Police Station");
+        Terminal.WriteLine("2 - State Highway Patrol Headquarters");
+        Terminal.WriteLine("3 - FBI Criminal Database");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter the ID of the chosen database to continue:");
+    }
+
+    public void ShowPoliceWinScreen()
+    {
+        Terminal.WriteLine(@"
+         ,   /\   ,
+        / '-'  '-' \
+       |   POLICE  |
+        \  .---.  /
+        | ( 121 ) |
+        \  '---'  /
+         '--. .--'
+             '");
+        Terminal.WriteLine("Police Database Accessed");
+        Terminal.WriteLine("Good Work Agent 47");
+        Terminal.WriteLine("Press 'enter' to play again");
+    }
+
+    public void ShowSHPWinScreen()
+    {
+        Terminal.WriteLine(@"
+..................................
+    _*_              _*_ 
+ __/_|_\__ ---    __/_|_\__ ---
+[(o)_R_(o)] ---  [(o)_R_(o)] ---
+..................................");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("State Highway System Accessed");
+        Terminal.WriteLine("Good Work Agent 47");
+        Terminal.WriteLine("Press 'enter' to play again");
+    }
+
+    public void ShowFBIWinScreen()
+    {
+        Terminal.WriteLine(@"
+**********************
+  ===== ====  ======
+  ||    || \\   ||
+  ||==  ||=<<   ||
+  ||    || //   ||
+  ==    ====  ======
+**********************");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("FBI Mainframe Accessed");
+        Terminal.WriteLine("Good Work Agent 47");
+        Terminal.WriteLine("Press 'enter' to play again");
+    }
+
+    public string ScrambleWord(string word)
+    {
+        char[] chars = new char[word.Length];
+        system.Random rand = new system.Random();
+        int index = 0;
+        while (word.Length > 0)
+        { // Get a random number between 0 and the length of the word. 
+            int next = rand.Next(0, word.Length - 1); // Take the character from the random position and add to our char array.
+            
+            chars[index] = word[next];                // Remove the character from the word. 
+            word = word.Substring(0, next) + word.Substring(next + 1);
+            ++index;
+        }
+        return new system.String(chars);
+    }
+}
